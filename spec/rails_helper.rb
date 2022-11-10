f3beec6ea8ad047d7a6a36a1d03f6399602fff49
immprada <im.mprada@gmail.com>
2022-11-10 08:57:41 -0500

    update by rubocop

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
-require "support/shoulda_matchers"
+require 'support/shoulda_matchers'
 require 'support/factory_bot'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
