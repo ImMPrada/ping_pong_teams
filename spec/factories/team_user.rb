ac4df27fd2252ccc0e6b13b0dfdc0af46646e387
Miguel Prada <mapra99@gmail.com>
2022-11-10 22:11:56 -0500

    add role field to team_users

--- spec/factories/team_user.rb
+++ spec/factories/team_user.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :team_user do
     association :team
     association :user
+
+    role { TeamUser::ROLES.values.sample }
   end
 end
--- spec/models/team_user_spec.rb
+++ spec/models/team_user_spec.rb
@@ -7,4 +7,8 @@ RSpec.describe TeamUser, type: :model do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to belong_to(:team) }
   end
+
+  describe 'Validations' do
+    it { is_expected.to validate_inclusion_of(:role).in_array(TeamUser::ROLES.values) }
+  end
 end
